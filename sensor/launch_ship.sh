d7305bc46649079842f4064fbbe200580c24a55c
Tim Garner <timgarner0@gmail.com>
2019-08-06 12:13:33 -0700

    Rework "ship" pod to support 3.3 sensor changes

---
+++ sensor/launch_ship.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# install the Tetration sensor and get a uuid
+rpm -Uvh --nodeps sensor.rpm
+echo "ACTIVATION_KEY=$2" > /usr/local/tet/user.cfg
+cd /usr/local/tet/ && ./fetch_sensor_id.sh
+
+# start the Tetration sensor
+cd /usr/local/tet && /usr/sbin/tet-engine tet-sensor -f conf/.sensor_config &
+cd /usr/local/tet && /usr/sbin/tet-engine -n -c tet-main &
+cd /usr/local/tet && /usr/sbin/tet-engine -n tet-enforcer --logtostderr &
+
+# launch the traffic generator
+cd / && mv ship $1; ./$1
