9294855c9d6ac630758be1146335919bc2c475b7
Tim Garner <timgarner0@gmail.com>
2019-11-05 10:51:08 -0800

    support https proxy

--- sensor/launch_ship.sh
+++ sensor/launch_ship.sh
@@ -3,6 +3,7 @@
 # install the Tetration sensor and get a uuid
 rpm -Uvh --nodeps sensor.rpm
 echo "ACTIVATION_KEY=$2" > /usr/local/tet/user.cfg
+echo "HTTPS_PROXY=$3" >> /usr/local/tet/user.cfg
 cd /usr/local/tet/ && ./fetch_sensor_id.sh
 
 # start the Tetration sensor
